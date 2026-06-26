d94ce8f04f891872c079ceff972da1dfc3d34dc6
Shoshi <evilseanbot@gmail.com>
2026-06-25 22:54:10 -0700

    Added candidates from henry, added plugins for pokemon like mechanics

---
+++ js/plugins/RemoveCommands.js
@@ -0,0 +1,11 @@
+(function() {
+    Window_ActorCommand.prototype.addAttackCommand = function() {
+        // This leaves the function blank, completely removing the attack option
+    };
+    Window_ActorCommand.prototype.addGuardCommand = function() {
+        // This leaves the function blank, completely removing the guard option
+    };
+    Window_ActorCommand.prototype.addItemCommand = function() {
+        // This leaves the function blank, completely removing the item option
+    };
+})();
